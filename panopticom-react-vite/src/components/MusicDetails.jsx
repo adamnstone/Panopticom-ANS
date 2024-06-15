f339bb6c8da2c714c986448f9079d15ceb0634d6
adam-stone <25stonea@charlottelatin.org>
2024-06-14 21:50:55 -0400

    radio station links use tagret='_blank' to open in new tab

--- panopticom-react-vite/src/components/MusicDetails.jsx
+++ panopticom-react-vite/src/components/MusicDetails.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import { marked } from 'marked'
 import DOMPurify from 'dompurify';
 
+const parseSanitizeModifyLinks = str => {
+    const innerHTML = new DOMParser().parseFromString(DOMPurify.sanitize(marked.parse(str)), 'text/html');
+    innerHTML.querySelectorAll("a").forEach(a => a.target = "_blank");
+    return innerHTML.body.innerHTML;
+}
+
 const MusicDetails = ({ musicDetails }) => {
     let title, description;
     if (musicDetails && musicDetails.title) {
-        title = <div className="fs-4" dangerouslySetInnerHTML={{ __html:DOMPurify.sanitize(marked.parse(musicDetails.title)) }}></div>;
+        title = <div className="fs-4" dangerouslySetInnerHTML={{ __html: parseSanitizeModifyLinks(musicDetails.title)}}></div>;
     } else {
         title = <div className="fs-4">View music details...</div>;
     }
 
     if (musicDetails && musicDetails.description) {
-        description = <div className="fs-6" dangerouslySetInnerHTML={{ __html:DOMPurify.sanitize(marked.parse(musicDetails.description)) }}></div>;
+        description = <div className="fs-6" dangerouslySetInnerHTML={{ __html:parseSanitizeModifyLinks(musicDetails.description) }}></div>;
     } else {
         description = <div className="fs-6">View description here...</div>
     }
