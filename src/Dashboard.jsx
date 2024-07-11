c1f12ea9f282dff1ae3318145746a32d960bab5a
adam-stone <25stonea@charlottelatin.org>
2024-07-11 11:29:37 -0400

    grid layout

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -7,16 +7,57 @@ const Dashboard = ({ layerData, LayerType }) => {
     <style>{`
     #three-container {
         width: 50vh; 
-        height: 50vh;
+        height: 48vh;
+        border-radius: 25px;
+        overflow: hidden;
+    }
+    .grid-item {
+        height: 48vh;
+        margin: 5px;
     }
     `}</style>
-    <div id="three-container">
-    <Three layerData={layerData} LayerType={LayerType} 
-    setHoverDetails={() => {}} 
-    setMusicDetails={() => {}} 
-    setFilterUpdateFunc={() => {}} 
-    openModal={() => {}} 
-    setStoryDetails={() => {}} />
+    <div className="container">
+        <div className="row">
+            <div className="col-4">
+                <div className="item-container bg-secondary grid-item align-items-center justify-content-center d-flex">
+
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="item-container grid-item align-items-center justify-content-center d-flex">
+                    <div id="three-container">
+                        <Three layerData={layerData} LayerType={LayerType} 
+                        setHoverDetails={() => {}} 
+                        setMusicDetails={() => {}} 
+                        setFilterUpdateFunc={() => {}} 
+                        openModal={() => {}} 
+                        setStoryDetails={() => {}} />
+                    </div>
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="item-container bg-secondary grid-item align-items-center justify-content-center d-flex">
+
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-4">
+                <div className="item-container bg-secondary grid-item align-items-center justify-content-center d-flex">
+
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="item-container bg-secondary grid-item align-items-center justify-content-center d-flex">
+
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="item-container bg-secondary grid-item align-items-center justify-content-center d-flex">
+
+                </div>
+            </div>
+        </div>
     </div>
     </>
   )
