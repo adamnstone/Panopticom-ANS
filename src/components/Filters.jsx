27a4b0faae7ab37003e0b9ecdf27f7d71bf1fe9f
Jean-michel Molenaar <jean-michel@fabfoundation.org>
2024-07-18 13:17:20 +0200

    some styling elements

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,6 +5,7 @@ import ActiveLayerFilters from './ActiveLayerFilters.jsx'
 
 const Filters = ({ hoverDetails, musicDetails, layerData, filterUpdateFunc }) => {
     const [checkedList, setCheckedList] = useState(layerData.map(l => true)); // all filters initially true, also must change in Three.jsx
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const onChangeCallback = (layerID, position) => {
         let changedLayerEnabled;
@@ -24,32 +25,43 @@ const Filters = ({ hoverDetails, musicDetails, layerData, filterUpdateFunc }) =>
         setCheckedList(updatedCheckedState);
     };
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <div className="card position-absolute top-50 translate-middle-y" style={{left: "1%", height: "85%", width: "29%", zIndex: 1000000}}>
-            <div className="card-body d-flex flex-column justify-content-center rounded border border-primary">
-                <div className="row my-auto">
-                    <div className="column">
-                    <div className="card">
-                            <div className="card-body rounded border border-secondary">
-                                <ActiveLayerFilters layerData={layerData} onChangeCallback={onChangeCallback} checkedList={checkedList} />
+        <div>
+            <div className={`hamburger-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <div className={`menu-container ${menuOpen ? 'open' : ''}`}>
+                <div className="card position-absolute top-49" style={{left: "1%", height: "85%", width: "100%", zIndex: 1000000}}>
+                    <div className="card-body d-flex flex-column justify-content-center rounded border border-primary">
+                        <div className="row my-auto">
+                            <div className="column">
+                                <div className="card">
+                                    <div className="card-body rounded border border-secondary">
+                                        <ActiveLayerFilters layerData={layerData} onChangeCallback={onChangeCallback} checkedList={checkedList} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row my-auto">
-                    <div className="column">
-                        <div className="card">
-                            <div className="card-body rounded border border-secondary">
-                                <HoverDetails hoverDetails={hoverDetails} />
+                        <div className="row my-auto">
+                            <div className="column">
+                                <div className="card">
+                                    <div className="card-body rounded border border-secondary">
+                                        <HoverDetails hoverDetails={hoverDetails} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row my-auto">
-                    <div className="column">
-                        <div className="card">
-                            <div className="card-body rounded border border-secondary">
-                                <MusicDetails musicDetails={musicDetails} />
+                        <div className="row my-auto">
+                            <div className="column">
+                                <div className="card">
+                                    <div className="card-body rounded border border-secondary">
+                                        <MusicDetails musicDetails={musicDetails} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
