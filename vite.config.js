878dd24ae1f111bd84ef2c2cab09bc693244ccc1
adam-stone <25stonea@charlottelatin.org>
2024-07-19 22:01:15 -0400

    config changed with gh-pages ext, dashboard.html removed

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { ghPages } from 'vite-plugin-gh-pages'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure the output directory is 'dist'
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [react(), ghPages()],
 })
