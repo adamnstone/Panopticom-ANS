70001378f80466ed3da326e2f451444d00e8cfba
adam-stone <25stonea@charlottelatin.org>
2024-07-20 12:22:21 -0400

    HashRouter -> BrowserRouter (main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import AINav from './AINav.jsx'
 import AIDashboard from './AIDashboard.jsx'
 import Dashboard from './Dashboard.jsx'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const LayerType = {
   STANDARD: 0,
