1c4569cf51e09bd337603c67a419bf3f9e446f7b
adam-stone <25stonea@charlottelatin.org>
2024-07-19 21:50:45 -0400

    vite changed --> routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the output directory is 'dist'
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
