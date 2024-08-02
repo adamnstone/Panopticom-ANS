afef1b1ccea1c0e815181ac21395247231836513
adam-stone <25stonea@charlottelatin.org>
2024-08-02 15:33:59 -0400

    fablab link added to image

--- src/components/OverlayText.jsx
+++ src/components/OverlayText.jsx
@@ -4,7 +4,7 @@ const OverlayText = () => {
   return (
     <div id="overlay-text-container">
         <a href="/about" id="about-link-text">About</a>
-        <img src="./fablab-img.png" id="fablab-icon"></img>
+        <img src="./fablab-img.png" id="fablab-icon" onClick={() => {window.open("https://www.fablabs.io/")}}></img>
         <p id="panopticom-txt" onClick={() => {window.open("https://petergabriel.com/release/panopticom/")}}>PANOPTICOM</p>
     </div>
   )
