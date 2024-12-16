2ffef48a26f3ba2250e2b652b8b862fab44eaa3e
adam-stone <25stonea@charlottelatin.org>
2024-12-16 08:25:58 -0500

    browser router reverted

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import About from './About.jsx'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 // enum for whether a dataset follows the JSONL Schema
 const LayerType = {
